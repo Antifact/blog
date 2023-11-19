7d11d218a67d8308a9017a2177c0337402c7a62d
Antifact <28497299+Antifact@users.noreply.github.com>
2023-11-19 20:16:03 +1000

    new blog. some styling added

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <div className='head'>
+      <div className='head-block'></div>
+      <div className='nav'>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#news">News</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <li><a href="#about">About</a></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
